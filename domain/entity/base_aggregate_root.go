9da79fccdb42cd02bf121c260e7908d119685f62
Phung Ngoc Duy <tonybka@gmail.com>
2023-01-04 23:27:23 +0800

    Entity, Aggregate Root and Domain Event

--- domain/entity/base_aggregate_root.go
+++ domain/entity/base_aggregate_root.go
@@ -4,8 +4,28 @@ import "github.com/tonybka/go-base-ddd/domain/event"
 
 type BaseAggregateRoot struct {
 	BaseEntity
+
+	domainEvents []event.IBaseDomainEvent
+}
+
+func NewBaseAggregateRoot() (BaseAggregateRoot, error) {
+	base, err := NewBaseEntity()
+	if err != nil {
+		return BaseAggregateRoot{}, nil
+	}
+
+	events := make([]event.IBaseDomainEvent, 0)
+
+	return BaseAggregateRoot{
+		BaseEntity:   base,
+		domainEvents: events,
+	}, nil
 }
 
 func (aggregate *BaseAggregateRoot) AddEvent(event event.IBaseDomainEvent) {
+	aggregate.domainEvents = append(aggregate.domainEvents, event)
+}
 
+func (aggregate *BaseAggregateRoot) PendingEvents() []event.IBaseDomainEvent {
+	return aggregate.domainEvents
 }
